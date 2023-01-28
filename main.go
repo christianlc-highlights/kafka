72ab16a50130bccc9fb12292b0e002d418a2c883
christian <callowaylc@gmail>
2023-01-28 01:22:08 -0500

    Add Golang Consumer

--- main.go
+++ main.go
@@ -1,41 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"os"
-
-	"github.com/christianlc-highlights/kafka/pkg"
-	log "github.com/sirupsen/logrus"
-
-)
-
-// main /////////////////////////////////////////
+import "github.com/christianlc-highlights/kafka/cmd"
 
 func main() {
-  logf := log.WithFields(
-    log.Fields{
-    	"trace": pkg.Trace("main", "main"),
-    	"args": os.Args,
-    },
-  )
-  logf.Debug("Enter")
-  defer logf.Debug("Exit")
-
-  if len(os.Args) < 2 {
-  	logf.Fatal("Failed to pass bootstrap server addresses")
-  }
-
-  p, err := pkg.Producer(os.Args...)
-  if err != nil {
-  	pkg.Logerr(logf, "Failed to create producer", err)
-  	panic(err)
-  }
-  logf.Info("Created kafka producer")
-
-  err = pkg.Write(p, "TopicTest", "The Message Payload")
-  if err != nil {
-  	pkg.Logerr(logf, "Failed to write to broker", err)
-  	panic(err)
-  }
-  logf.Info("Sent message to producer")
-
+	cmd.Execute()
 }
--- pkg/consumer.go
+++ pkg/consumer.go
@@ -1,1 +1,60 @@
 package pkg
+
+import (
+	"time"
+	"strings"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	log "github.com/sirupsen/logrus"
+)
+
+// create and return pointer to kafka consumer
+func Consumer(bootstrap ...string) (*kafka.Consumer, error) {
+  servers := strings.Join(bootstrap[1:], ",")
+  logf := log.WithFields(
+    log.Fields{
+    	"trace": Trace("Consumer", "pkg/consumer"),
+    	"bootstrap": servers,
+    },
+  )
+
+  logf.Debug("Enter")
+  defer logf.Debug("Exit")
+
+  return kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": servers,
+		"group.id": "test",
+		"auto.offset.reset": "earliest",
+  })
+
+}
+
+// Blocking read against kafka broker
+func Read(c *kafka.Consumer, topic string) (string, error) {
+  logf := log.WithFields(log.Fields{
+  	"trace": Trace("Read", "pkg/consumer"),
+  	"topic": topic,
+  })
+
+  logf.Debug("Enter")
+  defer logf.Debug("Exit")
+
+	msg, err := c.ReadMessage(time.Second)
+	if err != nil {
+		return "", err
+	}
+
+	return string(msg.Value), nil
+}
+
+// Determine if error is a read timeout
+func ErrIsTimeout(err error) bool {
+  logf := log.WithFields(log.Fields{
+  	"trace": Trace("ErrIsTimeout", "pkg/consumer"),
+  	"error": err,
+  })
+  logf.Debug("Enter")
+  defer logf.Debug("Exit")
+
+  return err.(kafka.Error).Code() == kafka.ErrTimedOut
+}
